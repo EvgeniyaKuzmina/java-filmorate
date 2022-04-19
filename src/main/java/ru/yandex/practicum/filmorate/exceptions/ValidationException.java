0f1b130feeee090c434d21d23006bf163ab62989
Evgeniya <vlasova.jen@mail.ru>
2022-04-19 19:35:28 +0300

    каркас Spring Boot приложения Filmorate style: изменила класс ValidationException, убрала из него метод checkName. Вместо этого добавила в класс ValidationExceptionFilm метод checkName для проверки названия фильма и в класс ValidationExceptionUser метод checkLogin для проверки логина пользователя. Соответственно удалили лишний тестовый класс ValidationExceptionTest

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
@@ -1,10 +1,5 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-import lombok.extern.slf4j.Slf4j;
-import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.User;
-
-@Slf4j
 public class ValidationException extends Exception {
 
     private final String messages;
